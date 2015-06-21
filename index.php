2daa181789c6c5b594922c159b2a0eb6286362a9
Kin Lane <info@apievangelist.com>
2015-06-20 21:30:33 -0700

    add all folders

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+include "header.php";
+
+
+$ThisFile = fopen("apis.json", "r");
+$ObjectText =  fread($ThisFile,filesize("apis.json"));
+
+$ObjectResult = json_decode($ObjectText,true);
+
+if(is_array($ObjectResult))
+  {
+
+	$name = "";
+	if(isset($ObjectResult['name']))
+		{
+   	$name = $ObjectResult['name'];
+   	}
+	
+	$description = "";
+	if(isset($ObjectResult['description']))
+   	{
+     	$description = $ObjectResult['description'];
+     	}		
+
+	if(isset($ObjectResult['image']))
+		{
+		$image = $ObjectResult['image'];
+		}			
+			
+	if(isset($ObjectResult['url']))
+		{
+		$url = $ObjectResult['url'];  // add as APIs.json - Authoritative
+		}
+
+	$includes = $ObjectResult['includes'];
+	
+	echo '<p><img src="' . $image . '" width="100" align="right" /></p>';
+	echo '<p><strong>' . $name . '</strong><br />';
+	echo '' . $description . '</p>';
+
+	?>
+	<p><strong><?php echo $SwaggerInfo_Title; ?></strong></p>
+	<ul>            
+	<?php
+	 foreach($includes as $include)
+	    {
+		$name = $include['name'];
+		$url = $include['url'];
+		 ?><li><a href="<?php echo $url; ?>"<?php echo $name; ?></a></li><?php
+		}
+		?>
+	  </ul>
+	  <?php				
+	}  
+
+include "footer.php";
+?>
